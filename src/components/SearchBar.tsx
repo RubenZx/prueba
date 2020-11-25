c920cfed1832fbfa10ba5bef61fd9d18867bec11
RubenZx <ruben.mondom@gmail.com>
2020-11-25 11:48:09 +0100

    :zap: removed icon button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,3 @@
-import IconButton from "@material-ui/core/IconButton";
 import InputBase from "@material-ui/core/InputBase";
 import Paper from "@material-ui/core/Paper";
 import SearchIcon from "@material-ui/icons/Search";
@@ -30,16 +29,16 @@ const SearchBar = ({ onValueChange }: SearcBarProps) => {
   return (
     <StyledPaper>
       <StyledInput
-        placeholder="Search user"
+        placeholder="Buscar usuario..."
         value={searchValue}
         onChange={(event) => {
           setSearchValue(event.target.value);
           onValueChange(event.target.value);
         }}
       />
-      <IconButton style={{ padding: 10 }} onClick={() => console.log("hi")}>
+      <div style={{ padding: 10 }}>
         <SearchIcon />
-      </IconButton>
+      </div>
     </StyledPaper>
   );
 };
