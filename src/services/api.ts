be2e949d1a0ae0bedd9ac5c708eede96123342cc
RubenZx <ruben.mondom@gmail.com>
2020-11-25 11:48:56 +0100

    :zap: added two functions for manage user by id

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,3 +7,19 @@ export const getUsers = async (): Promise<User[]> => {
   const res = await api.get("/users");
   return res.data;
 };
+
+export const getUserById = async (id: number): Promise<User> => {
+  const res = await api.get(`/users/${id}`);
+  return res.data;
+};
+
+export const editUserById = async (
+  id: number,
+  data: {
+    email: string;
+    phone: string;
+  }
+): Promise<any> => {
+  const res = await api.put(`/users/${id}`, { ...data });
+  return res.data;
+};
